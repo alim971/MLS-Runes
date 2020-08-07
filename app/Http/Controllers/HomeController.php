7df70e18d3e4045f687ceb44f04923b036d00100
Jozef Bugos <jozef.bugos@sap.com>
2020-08-07 13:39:11 +0200

    adding a error message, and fixing flash closing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Services\ChampionService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
@@ -20,7 +21,7 @@ class HomeController extends Controller
     public function __invoke(Request $request, ChampionService $championService)
     {
         //
-        if (!$request->secure() && !$this->app->environment('local')) {
+        if (!$request->secure() && App::environment() != ('local')) {
             flash()->error('You are accessing the page via unsecure HTTP. The site will not work for you, unless
              you use HTTPS.')->important();
         }
