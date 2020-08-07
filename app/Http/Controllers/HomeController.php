6aa56e5d2cb021099c67029d403d8788d772b0c3
Jozef Bugos <jozef.bugos@sap.com>
2020-08-07 21:05:57 +0200

    fixing error

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class HomeController extends Controller
@@ -22,14 +21,17 @@ class HomeController extends Controller
     public function __invoke(Request $request, ChampionService $championService)
     {
         //
-//        if (!$request->secure() && App::environment() != 'local') {
+//        if (!$request->secure() && App::environment() != ('local')) {
 //            flash()->error('You are accessing the page via unsecure HTTP. The site will not work for you, unless
 //             you use HTTPS.')->important();
 //        }
-        Log::debug('Request secure? ' . $request->secure());
-        if(!$championService->getChampions()) {
+        if(!$championService->loadChampions()) {
             flash()->error('Error loading newest data');
         }
-        return view('welcome');
+
+        $champions = $championService->getChampionsAlphabetically();
+        $roles = $championService->getRoles()->pluck('role');
+
+        return view('welcome', ['champions' => $champions ,'roles' => $roles]);
     }
 }
