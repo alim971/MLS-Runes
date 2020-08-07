4527d511a4ea01d97a4ca24afafab3040ac291be
Jozef Bugos <jozef.bugos@sap.com>
2020-08-07 12:30:52 +0200

    fixing typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -378,7 +378,7 @@
                 $('#base').val(data['base']);
                 $('#fight').val(data['fight']);
                 $('#after').val(data['after']);
-                $('#afterAll').val(data['afterAll' + ' (up to ' + $('#number').val()*10 + ' secs)']);
+                $('#afterAll').val(data['afterAll'] + ' (up to ' + $('#number').val()*10 + ' secs)');
                 $('#bonus').val(data['bonus'] + ' (' + data['after'] + ' total)');
             });
         }
