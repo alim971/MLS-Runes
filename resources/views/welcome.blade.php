3840ec7baa51b3cf23581a7f835516edfe9d98fb
Jozef Bugos <jozef.bugos@sap.com>
2020-08-09 15:45:44 +0200

    Changing aftershock and tankines to resistances, adding phase rush, comet, aery, klepto, glacial, showing scaling stats

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -334,7 +334,6 @@
 
                 if($('#role').val() == "Enchanter"){
                     $('#shieldDiv').removeClass('collapse');
-                    $('#shieldDiv').val($('minute').val() * 10);
                 } else if($('#role').val() != "Enchanter") {
                     if(!$('#shieldDiv').hasClass('collapse'))
                         $('#shieldDiv').addClass('collapse');
